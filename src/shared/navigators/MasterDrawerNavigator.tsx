718e28d53eae3d3fd001db86fcdeeba4a214d54e
Henrique Teles Ribeiro <henrique25.teles@hotmail.com>
2019-10-10 02:14:54 -0300

    Ícones nos menus principais do app

--- src/shared/navigators/MasterDrawerNavigator.tsx
+++ src/shared/navigators/MasterDrawerNavigator.tsx
@@ -6,31 +6,36 @@ import OrdersStackNavigator from './OrdersStackNavigator'
 import SettingsStackNavigator from './SettingsStackNavigator'
 import AboutStackNavigator from './AboutStackNavigator'
 import ThemeStackNavigator from './ThemeStackNavigator'
+import { Icon } from 'react-native-elements'
 
 export default function MasterDrawerNavigator() {
     const createNavigator = createDrawerNavigator({
         Orders: {
             screen: props => <OrdersStackNavigator toogleDrawer={props.navigation.toggleDrawer} />,
             navigationOptions: ({navigation}) => ({
-                drawerLabel: 'Orders'
+                drawerLabel: 'Orders',
+                drawerIcon: <Icon name="home" type="antdesign" />
             }),
         },
         Settings: {
             screen: SettingsStackNavigator,
             navigationOptions: ({navigation}) => ({
-                drawerLabel: 'Settings'
+                drawerLabel: 'Settings',
+                drawerIcon: <Icon name="settings" type="feather" />
             }),
         },
         About: {
             screen: AboutStackNavigator,
             navigationOptions: ({navigation}) => ({
-                drawerLabel: 'About'
+                drawerLabel: 'About',
+                drawerIcon: <Icon name="questioncircleo" type="antdesign" />
             }),
         },
         Theme: {
             screen: ThemeStackNavigator,
             navigationOptions: ({navigation}) => ({
-                drawerLabel: 'Theme'
+                drawerLabel: 'Theme',
+                drawerIcon: <Icon name="theme-light-dark" type="material-community" />,
             }),
         }
     })
