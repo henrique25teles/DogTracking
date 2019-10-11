e90eb1743eefc3f7444b0b93e4ff904de18569cb
Henrique Teles Ribeiro <henrique25.teles@hotmail.com>
2019-10-11 01:49:18 -0300

    Criação de esquema de cores e mudança na cor da status bar

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { StyleSheet, StatusBar, View } from 'react-native';
+
+import Color from './src/models/enums/Color';
 import Navigator from './src/shared/navigators/MasterDrawerNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Navigator />
-    </View>
+    <>
+      <StatusBar translucent backgroundColor={Color.DodgerBlue} />
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <Navigator />
+        </View>
+      </View>
+    </>
   );
 }
 
@@ -14,7 +21,12 @@ const styles = StyleSheet.create({
   container: {
     //marginTop: StatusBar.currentHeight,
     //marginBottom: StatusBar.currentHeight,
+    // height: '100%',
+    // width: '100%',
+    flex: 1
+  },
+  content: {
     height: '100%',
     width: '100%',
-  },
+  }
 });
--- src/shared/navigators/OrdersStackNavigator.tsx
+++ src/shared/navigators/OrdersStackNavigator.tsx
@@ -5,6 +5,7 @@ import {createMaterialTopTabNavigator} from 'react-navigation-tabs'
 import { TouchableOpacity } from 'react-native'
 import {Icon} from 'react-native-elements'
 
+import Color from '../../models/enums/Color'
 import Order from '../../pages/Order'
 import OrderDetails from '../../pages/OrderDetails'
 
@@ -44,18 +45,28 @@ export default function OrdersStackNavigator(props: Props) {
                 title: 'Orders',
                 headerLeft: () => (
                     <Icon 
-                        name="menuunfold" 
+                        name="menu-fold" 
                         type="antdesign" 
                         Component={TouchableOpacity}
+                        containerStyle={{paddingLeft: 5}}
+                        color={Color.GhostWhite}
                         onPress={() => props.toogleDrawer()} 
                     />
-                )
+                ),
+                headerStyle: {
+                    backgroundColor: Color.DodgerBlue,
+                },
+                headerTintColor: Color.GhostWhite,
             })
         },
         OrderDetail: {
             screen: OrderDetails,
-            navigationOptions: ({navigationOptions}) => ({
-                title: 'Order Details'
+            navigationOptions: ({navigation}) => ({
+                title: 'Order Details',
+                headerStyle: {
+                    backgroundColor: Color.DodgerBlue.toLowerCase(),
+                },
+                headerTintColor: Color.GhostWhite.toLowerCase(),
             })
         }
     })
