df1d9841ea47f7fd1527ca7b749e6f4af0a648b3
Henrique Teles Ribeiro <henrique25.teles@hotmail.com>
2019-10-17 01:16:02 -0300

    Remoção dos contexto de settings

--- src/interfaces/SettingsInterface.ts
+++ src/interfaces/SettingsInterface.ts
@@ -1,15 +1,4 @@
-
-
 export interface LanguageProps {
     id: string,
     name: string,
-}
-
-export interface Language {
-    selectedLanguage: LanguageProps,
-    languages: Array<LanguageProps>
-}
-
-export interface SettingsState {
-    language: Language,
 }
