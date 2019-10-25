b73802ca26c99b4cc73b229cb02e1e4c133614dc
Henrique Teles Ribeiro <henrique25.teles@hotmail.com>
2019-10-25 16:31:45 -0300

    Correção do componente localization

--- src/translations/Localization.ts
+++ src/translations/Localization.ts
@@ -5,7 +5,6 @@ import {LocalizedStrings} from 'interfaces/TranslationInterface'
 
 import en_US from 'translations/en-US'
 import pt_BR from 'translations/pt-BR'
-import { LanguageProps } from 'interfaces/SettingsInterface'
 
 const localizedStrings : LocalizedStrings = {
     "en": en_US,
@@ -18,11 +17,6 @@ const localizedStrings : LocalizedStrings = {
 i18n.fallbacks = true
 i18n.translations = localizedStrings
 
-export const defaultLocale = i18n.defaultLocale = 'en'
+i18n.defaultLocale = 'en'
 
-export const supportedLanguages : Array<LanguageProps> = [
-    {id: 'system', name: 'System', symbol:`${emoji.get(':round_pushpin:')}` },
-    {id: 'pt-BR', name: 'Português Brasileiro', symbol: `${emoji.get(':flag-br:')}`},
-    {id: 'en-US', name: 'English US', symbol: `${emoji.get(':us:')}`},
-]
 export default i18n
